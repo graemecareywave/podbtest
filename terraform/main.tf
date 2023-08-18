86ba996ad7a06276532bd329181112743fee8f97
dwgood78 <92883166+dwgood78@users.noreply.github.com>
2023-08-18 12:09:43 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,23 +42,3 @@ resource "google_monitoring_alert_policy" "internal_error_alert" {
 
   notification_channels = var.alert_channels
 }
-Variables.tf
-
-variable "project_id" {
-  description = "The project ID to deploy to"
-}
-
-variable "metric_name" {
-  description = "Name of the log-based metric."
-  default     = "compute_engine_internal_error_metric"
-}
-
-variable "alert_policy_name" {
-  description = "The display name for the alert policy."
-  default     = "Compute Engine Internal Error Alert"
-}
-
-variable "alert_channels" {
-  description = "List of notification channels in which the alert will be delivered"
-  type        = list(string)
-}

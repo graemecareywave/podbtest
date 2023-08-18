5068c97e46660e12ff26bfd33f8f3dfa0c40f9fd
Dwayne Gooden <dwayne.gooden@mavenwave.com>
2023-08-18 19:25:18 +0000

    updates to resolve the terraform apply errors

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,53 +1,61 @@
 provider "google" {
   # Resource google_monitoring_dashboard is available since 3.23.0
   # https://github.com/terraform-providers/terraform-provider-google/releases/tag/3.23
-  version = ">= 3.23.0"
-  source = "hashicorp/google"
   project = "starry-diode-376414"
   region  = "us-central1"
 }
 
-resource "google_logging_project_metric" "internal_error_metric" {
-  name        = var.metric_name
-  project     = var.project_id
-  description = "Count of logs with internal server error from compute engine."
-  filter      = "resource.type=gce_instance AND logName:\"projects/[PROJECT_ID]/logs/compute.googleapis.com%2Fsystem_event\" AND textPayload:\"*Status: 500 Response:nb'Internal Server Error\\n'\""
-  metric_descriptor {
-    metric_kind = "DELTA"
-    value_type  = "INT64"
-    labels {
-      key         = "log"
-      value_type  = "STRING"
-      description = "The log from which the metric was obtained."
-    }
-  }
+terraform {
+  required_providers {
+    google = {
+      version = "~> 3.83.0"
+    }
+  }
+}
+
+resource "google_logging_metric" "internal_error_metric" {
+  name        = var.metric_name
+  project     = var.project_id
+  description = "Count of logs with internal server error from compute engine."
+  filter      = "resource.type=gce_instance AND logName:\"projects/[PROJECT_ID]/logs/compute.googleapis.com%2Fsystem_event\" AND textPayload:\"*Status: 500 Response:nb'Internal Server Error\\n'\""
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type  = "INT64"
+    /*
+      labels {
+      key         = "log"
+      value_type  = "STRING"
+      description = "The log from which the metric was obtained."
+    }
+    */
+  }
 }
 
 resource "google_monitoring_alert_policy" "internal_error_alert" {
-  project      = var.project_id
-  display_name = var.alert_policy_name
-  combiner     = "OR"
+  project      = var.project_id
+  display_name = var.alert_policy_name
+  combiner     = "OR"
 
-  conditions {
-    display_name = "compute_engine_internal_error_condition"
-    condition_threshold {
-      filter     = "metric.type=\"logging.googleapis.com/user/${google_logging_project_metric.internal_error_metric.name}\""
-      duration   = "300s"
-      comparison = "COMPARISON_GT"
-      threshold_value = 1.0
+  conditions {
+    display_name = "compute_engine_internal_error_condition"
+    condition_threshold {
+      filter          = "metric.type=\"logging.googleapis.com/user/${google_logging_metric.internal_error_metric.name}\" AND resource.type=\"gce_instance\""
+      duration        = "300s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = 1.0
 
-      trigger {
-        count = 1
-      }
+      trigger {
+        count = 1
+      }
 
-      aggregations {
-        alignment_period   = "600s"
-        per_series_aligner = "ALIGN_RATE"
-        cross_series_reducer = "REDUCE_SUM"
-        group_by_fields     = ["instance_id"]
-      }
-    }
-  }
+      aggregations {
+        alignment_period     = "600s"
+        per_series_aligner   = "ALIGN_RATE"
+        cross_series_reducer = "REDUCE_SUM"
+        group_by_fields      = ["resource.labels.instance_id"]
+      }
+    }
+  }
 
-  notification_channels = var.alert_channels
+  notification_channels = var.alert_channels
 }

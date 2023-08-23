2c3554f52d9dc534f0ecc8ec937531de4a64a0dc
idris.yatou <idris.yatou@mavenwave.com>
2023-08-23 12:14:58 -0500

    added new metric and alert policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,6 +31,53 @@ resource "google_logging_metric" "internal_error_metric" {
   }
 }
 
+resource "google_logging_metric" "internal_error_metric2" {
+  name        = var.metric_name
+  project     = var.project_id
+  description = "Count of logs with internal server error from compute engine."
+  filter      = "resource.type=gce_instance AND logName:\"projects/[PROJECT_ID]/logs/compute.googleapis.com%2Fsystem_event\" AND textPayload:\"404\""
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type  = "INT64"
+    /*
+      labels {
+      key         = "log"
+      value_type  = "STRING"
+      description = "The log from which the metric was obtained."
+    }
+    */
+  }
+}
+
+resource "google_monitoring_alert_policy" "internal_error_alert2" {
+  project      = var.project_id
+  display_name = var.alert_policy_name
+  combiner     = "OR"
+
+  conditions {
+    display_name = "compute_engine_internal_error_condition"
+    condition_threshold {
+      filter          = "metric.type=\"logging.googleapis.com/user/${google_logging_metric.internal_error_metric2.name}\" AND resource.type=\"gce_instance\""
+      duration        = "300s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = 1.0
+
+      trigger {
+        count = 1
+      }
+
+      aggregations {
+        alignment_period     = "600s"
+        per_series_aligner   = "ALIGN_RATE"
+        cross_series_reducer = "REDUCE_SUM"
+        group_by_fields      = ["resource.labels.instance_id"]
+      }
+    }
+  }
+
+  notification_channels = var.alert_channels
+}
+
 resource "google_monitoring_alert_policy" "internal_error_alert" {
   project      = var.project_id
   display_name = var.alert_policy_name
